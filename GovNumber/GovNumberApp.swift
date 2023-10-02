593442916691647edfd9b2b674eb24e26511f544
Evpator1979 <evpator16061979@gmail.com>
2023-10-02 15:14:16 +0300

    Initial Commit

---
+++ GovNumber/GovNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GovNumberApp.swift
+//  GovNumber
+//
+//  Created by Dmitry Averin on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct GovNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
